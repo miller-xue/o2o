3752fdd570124fc151c36e36e037991379488351
miller_xue <1021362065@qq.com>
2019-03-17 22:15:54 +0800

    分类删除

--- src/main/java/com/miller/o2o/web/shopadmin/ProductCategoryController.java
+++ src/main/java/com/miller/o2o/web/shopadmin/ProductCategoryController.java
@@ -9,10 +9,7 @@ import com.miller.o2o.service.ProductCategoryService;
 import com.miller.o2o.util.HttpContextUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,6 +45,20 @@ public class ProductCategoryController {
             return AjaxResult.success();
         }
         return AjaxResult.error(execution.getStateInfo());
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/delete/{id}",method = RequestMethod.GET)
+    public AjaxResult delete(@PathVariable("id") Long id){
+        // TODO
+        if (id == null || id <= 0) {
+            return AjaxResult.error().state(ProductCategoryStateEnum.INNER_ERROR);
+        }
+        ProductCategoryExecution se = productCategoryService.delete(id);
+        if (se.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+            return AjaxResult.success();
+        }
+        return AjaxResult.error(se.getStateInfo());
     }
 
 }
